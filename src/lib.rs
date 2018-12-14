2417a6060a3963be20697880512309d7d7ec917d
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-12-14 01:14:40 -0500

    Update to nightly

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@
 //! [![Build Status](https://travis-ci.org/jeffrey-xiao/kademlia-dht-rs.svg?branch=master)](https://travis-ci.org/jeffrey-xiao/kademlia-dht-rs)
 //! [![codecov](https://codecov.io/gh/jeffrey-xiao/kademlia-dht-rs/branch/master/graph/badge.svg)](https://codecov.io/gh/jeffrey-xiao/kademlia-dht-rs)
 //!
-//! A flexible implementation of the Kademlia distributed hash table. This library crate was mainly created to better understand the Rust concurrency primitives. This implementation is fairly close to the spec described in the original Kademlia paper with the exception of a few design considerations.
+//! A flexible implementation of the Kademlia distributed hash table. This library crate was mainly
+//! created to better understand the Rust concurrency primitives. This implementation is fairly
+//! close to the spec described in the original Kademlia paper with the exception of a few design
+//! considerations.
 //!
 //! ## Examples
 //!
@@ -65,10 +68,16 @@
 //!
 //! ## Design Considerations
 //!
-//!  - Many of the paper's original optimizations were not implemented due to their complexity for arguably little gain.
-//!  - Each node's routing table uses a growable vector to represent the binary tree of k-buckets. The vector grows as the k-bucket closest to the node's ID exceeds capacity. The relaxation of k-bucket splitting proposed in Section 4.2 is not implemented.
-//!  - Caching and key republishing described in Section 2.5 is not implemented to simplify the number of moving parts and active threads. It is up to the user of the library to ensure that keys are being republished.
-//!  - The recursive lookup of nodes uses strict parallelism to tightly bound the number of active RPCs rather than the loose parallelism implied by the paper.
+//!  - Many of the paper's original optimizations were not implemented due to their complexity for
+//!    arguably little gain.
+//!  - Each node's routing table uses a growable vector to represent the binary tree of k-buckets.
+//!    The vector grows as the k-bucket closest to the node's ID exceeds capacity. The relaxation of
+//!    k-bucket splitting proposed in Section 4.2 is not implemented.
+//!  - Caching and key republishing described in Section 2.5 is not implemented to simplify the
+//!    number of moving parts and active threads. It is up to the user of the library to ensure that
+//!    keys are being republished.
+//!  - The recursive lookup of nodes uses strict parallelism to tightly bound the number of active
+//!    RPCs rather than the loose parallelism implied by the paper.
 //!  - Each key is 256 bits as opposed to 160 bits so that consumers can use SHA-3 instead of SHA-1.
 //!
 //! ## Changelog
@@ -77,13 +86,14 @@
 //!
 //! ## References
 //!
-//!  - [Kademlia: A Peer-to-Peer Information System Based on the XOR Metric](https://dl.acm.org/citation.cfm?id=687801)
+//!  - [Kademlia: A Peer-to-Peer Information System Based on the XOR
+//!    Metric](https://dl.acm.org/citation.cfm?id=687801)
 //!  > Maymounkov, Petar, and David Mazières. 2002. “Kademlia: A Peer-to-Peer Information System Based on the Xor Metric.” In *Revised Papers from the First International Workshop on Peer-to-Peer Systems*, 53–65. IPTPS ’01. London, UK, UK: Springer-Verlag. <http://dl.acm.org/citation.cfm?id=646334.687801>.
 //!
 //! ## License
 //!
-//! `kademlia-dht-rs` is dual-licensed under the terms of either the MIT License or the Apache License
-//! (Version 2.0).
+//! `kademlia-dht-rs` is dual-licensed under the terms of either the MIT License or the Apache
+//! License (Version 2.0).
 //!
 //! See [LICENSE-APACHE](LICENSE-APACHE) and [LICENSE-MIT](LICENSE-MIT) for more details.
 
